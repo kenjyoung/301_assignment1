d574ffea6870d5f09f9cfec9d51238d76e8cd740
Mike <kjyoung@ualberta.ca>
2015-01-25 13:09:12 -0700

    All planned UI screens are now roughly layed out.

--- gen/ca/ualberta/cs/kjyoung_expenses/R.java
+++ gen/ca/ualberta/cs/kjyoung_expenses/R.java
@@ -25,20 +25,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000d;
-        public static final int addClaimButton=0x7f08000a;
-        public static final int addExpenseButton=0x7f080002;
-        public static final int claimName=0x7f080001;
-        public static final int claimsList=0x7f08000b;
-        public static final int deleteClaimsButton=0x7f08000c;
-        public static final int deleteExpensesButton=0x7f080003;
-        public static final int editClaimButton=0x7f080004;
-        public static final int endDate=0x7f080008;
-        public static final int expensesList=0x7f080000;
-        public static final int from=0x7f080005;
-        public static final int startDate=0x7f080006;
-        public static final int to=0x7f080007;
-        public static final int travelClaims=0x7f080009;
+        public static final int action_settings=0x7f08001a;
+        public static final int addClaimButton=0x7f080019;
+        public static final int addExpenseButton=0x7f080012;
+        public static final int categoryHeader=0x7f080008;
+        public static final int categorySelector=0x7f080009;
+        public static final int claimDescriptionBody=0x7f080002;
+        public static final int claimDescriptionHeader=0x7f080001;
+        public static final int claimName=0x7f080010;
+        public static final int claimsList=0x7f080018;
+        public static final int currencyBody=0x7f08000d;
+        public static final int currencyHeader=0x7f08000c;
+        public static final int currencySelector=0x7f08000e;
+        public static final int deleteClaimsButton=0x7f080017;
+        public static final int deleteExpensesButton=0x7f080011;
+        public static final int descriptionBody=0x7f08000b;
+        public static final int descriptionHeader=0x7f08000a;
+        public static final int editClaimButton=0x7f080013;
+        public static final int endDate=0x7f080006;
+        public static final int endDateHeader=0x7f080005;
+        public static final int expensesList=0x7f08000f;
+        public static final int from=0x7f080014;
+        public static final int saveClaimButton=0x7f080000;
+        public static final int saveExpenseButton=0x7f080007;
+        public static final int startDate=0x7f080004;
+        public static final int startDateHeader=0x7f080003;
+        public static final int to=0x7f080015;
+        public static final int travelClaims=0x7f080016;
     }
     public static final class layout {
         public static final int activity_edit_claim=0x7f030000;
