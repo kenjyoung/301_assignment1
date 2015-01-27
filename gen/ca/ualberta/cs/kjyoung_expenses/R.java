ce8dd9afd885111aeb125be2ed363835a5af818a
Mike <kjyoung@ualberta.ca>
2015-01-26 21:14:06 -0700

    reformatted the claim editing menu to use a scroll view and added a (currently useless) status spinner

--- gen/ca/ualberta/cs/kjyoung_expenses/R.java
+++ gen/ca/ualberta/cs/kjyoung_expenses/R.java
@@ -8,6 +8,9 @@
 package ca.ualberta.cs.kjyoung_expenses;
 
 public final class R {
+    public static final class array {
+        public static final int status_spinner_entries=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,33 +28,35 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001a;
-        public static final int addClaimButton=0x7f080019;
-        public static final int addExpenseButton=0x7f080012;
-        public static final int categoryHeader=0x7f080008;
-        public static final int categorySelector=0x7f080009;
-        public static final int claimDescriptionBody=0x7f080002;
-        public static final int claimDescriptionHeader=0x7f080001;
-        public static final int claimName=0x7f080010;
-        public static final int claimsList=0x7f080018;
-        public static final int currencyBody=0x7f08000d;
-        public static final int currencyHeader=0x7f08000c;
-        public static final int currencySelector=0x7f08000e;
-        public static final int deleteClaimsButton=0x7f080017;
-        public static final int deleteExpensesButton=0x7f080011;
-        public static final int descriptionBody=0x7f08000b;
-        public static final int descriptionHeader=0x7f08000a;
-        public static final int editClaimButton=0x7f080013;
-        public static final int endDate=0x7f080006;
-        public static final int endDateHeader=0x7f080005;
-        public static final int expensesList=0x7f08000f;
-        public static final int from=0x7f080014;
-        public static final int saveClaimButton=0x7f080000;
-        public static final int saveExpenseButton=0x7f080007;
-        public static final int startDate=0x7f080004;
-        public static final int startDateHeader=0x7f080003;
-        public static final int to=0x7f080015;
-        public static final int travelClaims=0x7f080016;
+        public static final int action_settings=0x7f09001c;
+        public static final int addClaimButton=0x7f09001b;
+        public static final int addExpenseButton=0x7f090014;
+        public static final int categoryHeader=0x7f09000a;
+        public static final int categorySelector=0x7f09000b;
+        public static final int claimDescriptionBody=0x7f090003;
+        public static final int claimDescriptionHeader=0x7f090005;
+        public static final int claimName=0x7f090012;
+        public static final int claimsList=0x7f09001a;
+        public static final int currencyBody=0x7f09000f;
+        public static final int currencyHeader=0x7f09000e;
+        public static final int currencySelector=0x7f090010;
+        public static final int deleteClaimsButton=0x7f090019;
+        public static final int deleteExpensesButton=0x7f090013;
+        public static final int descriptionBody=0x7f09000d;
+        public static final int descriptionHeader=0x7f09000c;
+        public static final int editClaimButton=0x7f090015;
+        public static final int endDate=0x7f090008;
+        public static final int endDateHeader=0x7f090004;
+        public static final int expensesList=0x7f090011;
+        public static final int from=0x7f090016;
+        public static final int saveClaimButton=0x7f090000;
+        public static final int saveExpenseButton=0x7f090009;
+        public static final int startDate=0x7f090007;
+        public static final int startDateHeader=0x7f090006;
+        public static final int statusHeader=0x7f090001;
+        public static final int statusSpinner=0x7f090002;
+        public static final int to=0x7f090017;
+        public static final int travelClaims=0x7f090018;
     }
     public static final class layout {
         public static final int activity_edit_claim=0x7f030000;
@@ -61,10 +66,10 @@ public final class R {
         public static final int claim_list_item=0x7f030004;
     }
     public static final class menu {
-        public static final int expense_edit=0x7f070000;
-        public static final int expenses_list=0x7f070001;
-        public static final int main=0x7f070002;
-        public static final int new_claim=0x7f070003;
+        public static final int expense_edit=0x7f080000;
+        public static final int expenses_list=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int new_claim=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -103,10 +108,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
